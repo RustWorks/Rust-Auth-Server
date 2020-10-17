361a1f7c86707c47f4b88998201af7f5091a8dda
mygnu <tech@gill.net.in>
2020-10-17 12:04:08 +0100

    fix security issue

--- src/register_handler.rs
+++ src/register_handler.rs
@@ -8,6 +8,7 @@ use crate::utils::hash_password;
 // UserData is used to extract data from a post request by the client
 #[derive(Debug, Deserialize)]
 pub struct UserData {
+    pub email: String,
     pub password: String,
 }
 
@@ -16,14 +17,8 @@ pub async fn register_user(
     user_data: web::Json<UserData>,
     pool: web::Data<Pool>,
 ) -> Result<HttpResponse, ServiceError> {
-    let res = web::block(move || {
-        query(
-            invitation_id.into_inner(),
-            user_data.into_inner().password,
-            pool,
-        )
-    })
-    .await;
+    let res =
+        web::block(move || query(invitation_id.into_inner(), user_data.into_inner(), pool)).await;
 
     match res {
         Ok(user) => Ok(HttpResponse::Ok().json(&user)),
@@ -36,16 +31,17 @@ pub async fn register_user(
 
 fn query(
     invitation_id: String,
-    password: String,
+    user_data: UserData,
     pool: web::Data<Pool>,
 ) -> Result<SlimUser, crate::errors::ServiceError> {
-    use crate::schema::invitations::dsl::{id, invitations};
+    use crate::schema::invitations::dsl::{email, id, invitations};
     use crate::schema::users::dsl::users;
     let invitation_id = uuid::Uuid::parse_str(&invitation_id)?;
 
     let conn: &PgConnection = &pool.get().unwrap();
     invitations
         .filter(id.eq(invitation_id))
+        .filter(email.eq(&user_data.email))
         .load::<Invitation>(conn)
         .map_err(|_db_error| ServiceError::BadRequest("Invalid Invitation".into()))
         .and_then(|mut result| {
@@ -53,7 +49,7 @@ fn query(
                 // if invitation is not expired
                 if invitation.expires_at > chrono::Local::now().naive_local() {
                     // try hashing the password, else return the error that will be converted to ServiceError
-                    let password: String = hash_password(&password)?;
+                    let password: String = hash_password(&user_data.password)?;
                     dbg!(&password);
                     let user = User::from_details(invitation.email, password);
                     let inserted_user: User =
